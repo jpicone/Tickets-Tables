4f65929774caf3ac7af5179842cd5a6d58c67b47
Julian Picone <saasbook@saasbook.(none)>
2015-04-03 20:35:35 -0600

    Final commit of ITER2-2

---
+++ Project/reservationV4/db/migrate/20111119180638_create_restaurants.rb
@@ -0,0 +1,17 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def up
+    create_table :restaurants do |t|
+      t.string :restaurant_name
+      t.string :store_ID
+      t.string :phone_number
+      t.string :address
+      # Add fields that let Rails automatically keep track
+      # of when project tasks are added or modified:
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :restaurants
+  end
+end
